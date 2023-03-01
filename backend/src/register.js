84d95a470a60ceec5a4fed19b1a88a2904a44a16
Spady7702 <brenekspademan@yahoo.com>
2023-03-01 09:44:06 -0700

    Added more for register

--- backend/src/register.js
+++ backend/src/register.js
@@ -1,5 +1,18 @@
 class Register{
     static registerAccount(username, password, skills){
-        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (emailRegex.test(username)){
+            
+            if (password.length >= 4 && password.length <= 16){
+                // register the account 
+            }
+            else{
+                // let them know the password must be between 4 and 16 characters
+            }
+        }
+        else {
+            // let them know the username is not a valid email
+        }
     }
 }
