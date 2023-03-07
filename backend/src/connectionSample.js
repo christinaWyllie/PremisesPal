5dc55fb5b21a5557d4853b3c63da254ec11d0161
Arion Hamel <arionghamel@gmail.com>
2023-03-07 13:56:31 -0700

    Edited sample, and created login microservice

--- backend/src/connectionSample.js
+++ backend/src/connectionSample.js
@@ -1,54 +1,53 @@
-//create a connection to MYSQL
+//Grab mysql libraries
 const mysql = require('mysql');
 
 class DBConnection{    
-    connection;
+    
+    // COPY THIS INTO EACH MICROSERVICE
+    // == Abstracted query function that runs synchronously using promises ==
+    static async query( query ) {
 
-    // Establishes database connection upon new instance creation
-    constructor(){
-        this.connection = mysql.createConnection({
+        // -- Database connection --
+        var connection = mysql.createConnection({   // <== connection is used to establish connection with database
             host: 'localhost',
             user: 'seng401',
             password: 'pal', 
             database: 'PremisesPal'
         })
         
-        this.connection.connect(function(err) {
+        connection.connect(function(err) {
             if(err) {
                 return console.error('Unable to connect to database' + err.message);
             }
             console.log('Connection created successfully!');
         });
-    }
+        // -------------------------
 
-    // Abstracted query function that runs synchronously using promises
-    async query( query ) {
-        return new Promise((resolve,reject)=>{
-            this.connection.query( query, function(err,results,fields) {
-                if ( err ) reject(err) // rejections are for query errors, network and other failures
-                else resolve(results,fields) // resolve with query results
+        return new Promise(( resolve,reject )=>{
+            console.log(`Running query: ${query}`)
+
+            connection.query( query, function(err,results,fields) {
+                if ( err ) reject( err ) // rejections are for query errors, network and other failures
+                else resolve( results,fields ) // resolve with query results
             })    
         })
     }
+    // ======================================================================
     
     // login business logic - find a matching user and verify their password
-    async login( email, pswd ) {
+    static async login( email, pswd ) {
         console.log(`Querying for ${email} ${pswd}`)
-        const existingUsers = await this.query(`SELECT email, pass FROM ACCOUNT WHERE email ='${email}'`)
+        const existingUsers = await DBConnection.query(`SELECT email, pass FROM ACCOUNT WHERE email ='${email}'`)
         console.log('Found existing users', existingUsers)
+
         return (existingUsers && existingUsers.length && existingUsers[0].pass === pswd)
     }
 }
 
-console.log("Testing login query")
-
 async function testExists() {
-
-    var testDBConnection = new DBConnection();
-
     // busines logic to test login 
     console.log('/login with query')
-    const exists = await testDBConnection.login( 'arion@yahoo.ca', 'passwrd') 
+    const exists = await DBConnection.login( 'arion@yahoo.ca', 'passwrd') 
     console.log('DBConnection.login returned', exists)
 }
 
--- backend/src/login.js
+++ backend/src/login.js
@@ -1,13 +1,75 @@
+//Grab mysql libraries
+const mysql = require('mysql');
+
 class Login{
-    static sendUserPass(username, password){
-        if (Connection.validateUsername(username) == false){
-            // Message to login screen that username is not registered
-            return;
-        }
-        if (Connection.validatePassword(password) == false){
-            // Message to login screen password is incorrect
-            return;
-        }
-        // login user
+
+    // == Abstracted query function that runs synchronously using promises ==
+    static async query( query ) {
+
+        // -- Database connection --
+        var connection = mysql.createConnection({   // <== connection is used to establish connection with database
+            host: 'localhost',
+            user: 'seng401',
+            password: 'pal', 
+            database: 'PremisesPal'
+        })
+        
+        connection.connect(function(err) {
+            if(err) {
+                return console.error('Unable to connect to database' + err.message);
+            }
+            console.log('Connection created successfully!');
+        });
+        // -------------------------
+
+        return new Promise(( resolve,reject )=>{
+            console.log(`Running query: ${query}`)
+
+            connection.query( query, function(err,results,fields) {
+                if ( err ) reject( err ) // rejections are for query errors, network and other failures
+                else resolve( results,fields ) // resolve with query results
+            })    
+        })
+    }
+    // ======================================================================
+
+    // == Ensures username is present in ACCOUNT table
+    static async validateUsername( user ) {
+        console.log(`validateUsername: Validating ${user}`)
+        const existingUsers = await Login.query(`SELECT email FROM ACCOUNT WHERE email ='${user}'`)
+        console.log('validateUsername: Found existing users', existingUsers)
+        return (existingUsers && existingUsers.length && existingUsers[0].email === user)  // existingUsers is defined and nonzero
+    }
+
+    // == Ensures pass corresponds to given user in ACCOUNT table
+    static async validatePassword( user, pass ) {
+        console.log(`validatePassword: validating ${pass} for ${user}`)
+        const existingUsers = await Login.query(`SELECT email, pass FROM ACCOUNT WHERE email ='${user}'`)
+        console.log('validatePassword: Found existing users', existingUsers)
+        return (existingUsers && existingUsers.length && existingUsers[0].pass === pass) // existingUsers is defined, nonzero, and pass matches
     }
 }
+
+/**
+ * UNCOMMENT BELOW IN ORDER TO TEST ABOVE FUNCTIONALITY. Run using 'node login.js'
+ * An example of how to utilize this microserve (for Jack & Brenek)
+ */
+
+// async function mockLoginFunction() {
+//     console.log("\nMocking login functionality:")
+//     var username = await Login.validateUsername('arion@yahoo.ca')   // <= username to be validated
+//     console.log(`validateUsername returned: ${username}`)
+
+//     if(username) {
+//         console.log("! Email is registered\n")
+
+//         var password = await Login.validatePassword('arion@yahoo.ca', 'password')   // <= username and associated password to be validated
+//         console.log(`validatePassword returned: ${password}`)
+
+//         if(password) console.log("! Login successful\n")
+//         else console.log("! Password is incorrect - does not match\n")
+
+//     } else console.log("! Email is not registered\n")
+// }
+
+// mockLoginFunction()
