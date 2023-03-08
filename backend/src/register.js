09736c611938d8c984efebfedc3a3a6ad4c6683a
Jack Rovere <jackrovere@gmail.com>
2023-03-08 07:58:40 -0700

    update local branch

--- backend/src/register.js
+++ backend/src/register.js
@@ -4,10 +4,10 @@ class Register{
 
         if (emailRegex.test(email)){
             
-            //if (email exists) {
-                //console.log("email is already in use dumguy!");
-                //return false;
-            //}
+            if (validateUsername(email)) {
+                console.log("email is already in use dumguy!");
+                return false;
+            }
 
             if (password.length < 4 || password.length > 16){
                 console.log("password must be between 4 and 16 characters.");
