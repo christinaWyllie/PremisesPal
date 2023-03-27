c8d40bf7e54526a6cec3b7974955b520d6cb6bc9
Spady7702 <brenekspademan@yahoo.com>
2023-03-26 22:14:43 -0600

    Actually fixed bug in register

--- public/index.js
+++ public/index.js
@@ -76,45 +76,45 @@ app.post('/Register', async (req,res) => {
 	if (registrationResult == true) {
 		console.log("registration successful, redirecting...");
 		req.session.user = { email };
+
+		const userType = req.body.role;
+		if (userType == "homeOwner"){
+			// need to implement registration as a home owner
+			res.status(302).redirect('feed.html');
+		}
+		else if (userType == "contractor"){
+			const contractorRegistrationResult = await ContractorDB.addContractor(email, "Default biography");
+			if (contractorRegistrationResult){
+				// checks whether skills have been checked or not
+				const carpentrySkill = req.body.carpentry !== undefined;
+				if (carpentrySkill) await ContractorDB.addSkill(email, "Carpentry");
+				const plumbingSkill = req.body.plumbing !== undefined;
+				if (plumbingSkill) await ContractorDB.addSkill(email, "Plumbing");
+				const cleaningSkill = req.body.cleaning !== undefined;
+				if (cleaningSkill) await ContractorDB.addSkill(email, "Cleaning");
+				const electricalSkill = req.body.electrical !== undefined;
+				if (electricalSkill) await ContractorDB.addSkill(email, "Electrical");
+				const landscapingSkill = req.body.landscaping !== undefined;
+				if (landscapingSkill) await ContractorDB.addSkill(email, "Landscaping");
+				const paintingSkill = req.body.painting !== undefined;
+				if (paintingSkill) await ContractorDB.addSkill(email, "Painting");
+				const otherSkill = req.body.other !== undefined;
+				if (otherSkill) await ContractorDB.addSkill(email, "Other");
+				res.status(302).redirect('feed.html');
+			}
+			else {
+				console.log("Was unable to register as a contractor");
+			}
+		}
+		else{
+			// should never get to this else statement as the user selects homeOwner or contractor
+			// from a dropdown menu
+			console.log("Issue with the user type");
+		}
 	} else {
 		console.log("registration unsuccessful");
 		res.status(302).redirect('Register.html');
 		//need to add error message somehow
-	}
-
-	const userType = req.body.role;
-	if (userType == "homeOwner"){
-		// need to implement registration as a home owner
-		res.status(302).redirect('feed.html');
-	}
-	else if (userType == "contractor"){
-		const contractorRegistrationResult = await ContractorDB.addContractor(email, "Default biography");
-		if (contractorRegistrationResult){
-			// checks whether skills have been checked or not
-			const carpentrySkill = req.body.carpentry !== undefined;
-			if (carpentrySkill) await ContractorDB.addSkill(email, "Carpentry");
-			const plumbingSkill = req.body.plumbing !== undefined;
-			if (plumbingSkill) await ContractorDB.addSkill(email, "Plumbing");
-			const cleaningSkill = req.body.cleaning !== undefined;
-			if (cleaningSkill) await ContractorDB.addSkill(email, "Cleaning");
-			const electricalSkill = req.body.electrical !== undefined;
-			if (electricalSkill) await ContractorDB.addSkill(email, "Electrical");
-			const landscapingSkill = req.body.landscaping !== undefined;
-			if (landscapingSkill) await ContractorDB.addSkill(email, "Landscaping");
-			const paintingSkill = req.body.painting !== undefined;
-			if (paintingSkill) await ContractorDB.addSkill(email, "Painting");
-			const otherSkill = req.body.other !== undefined;
-			if (otherSkill) await ContractorDB.addSkill(email, "Other");
-			res.status(302).redirect('feed.html');
-		}
-		else {
-			console.log("Was unable to register as a contractor");
-		}
-	}
-	else{
-		// should never get to this else statement as the user selects homeOwner or contractor
-		// from a dropdown menu
-		console.log("Issue with the user type");
 	}
 
 });
